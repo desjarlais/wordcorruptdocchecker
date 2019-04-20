dd25542dd75da2a59f9ace7c9234bebc34aa7626
Brandon Desjarlais <brandondesjarlais@outlook.com>
2019-04-20 14:42:16 -0500

    - added new app icon - fixed open in Word after repair

--- DocCorruptionChecker/FrmMain.cs
+++ DocCorruptionChecker/FrmMain.cs
@@ -264,12 +264,6 @@ namespace DocCorruptionChecker
                                         lstOutput.Items.Add("-------------------------------------------------------------");
                                         lstOutput.Items.Add("Fixed Document Location: " + StrDestFileName);
                                         IsFixed = true;
-
-                                        // open the file in Word
-                                        if (Properties.Settings.Default.OpenInWord == "true")
-                                        {
-                                            Process.Start(StrDestFileName);
-                                        }
                                     }
                                 }
                             }
@@ -316,6 +310,12 @@ namespace DocCorruptionChecker
                     // check if we can open in the sdk and confirm it was indeed fixed
                     lstOutput.Items.Add("");
                     OpenWithSdk(StrDestFileName);
+                }
+
+                // open the file in Word
+                if (Properties.Settings.Default.OpenInWord == "true")
+                {
+                    Process.Start(StrDestFileName);
                 }
 
                 // need to reset the globals 
--- DocCorruptionChecker/frmMain.Designer.cs
+++ DocCorruptionChecker/frmMain.Designer.cs
@@ -42,10 +42,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(18, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "File path:";
             // 
@@ -58,19 +58,19 @@
             // 
             // tbxFileName
             // 
-            this.tbxFileName.Location = new System.Drawing.Point(130, 12);
-            this.tbxFileName.Margin = new System.Windows.Forms.Padding(6);
+            this.tbxFileName.Location = new System.Drawing.Point(98, 9);
+            this.tbxFileName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxFileName.Name = "tbxFileName";
-            this.tbxFileName.Size = new System.Drawing.Size(1042, 31);
+            this.tbxFileName.Size = new System.Drawing.Size(782, 26);
             this.tbxFileName.TabIndex = 1;
             // 
             // btnBrowse
             // 
             this.btnBrowse.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnBrowse.Location = new System.Drawing.Point(1188, 8);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBrowse.Location = new System.Drawing.Point(891, 6);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(88, 44);
+            this.btnBrowse.Size = new System.Drawing.Size(66, 33);
             this.btnBrowse.TabIndex = 2;
             this.btnBrowse.Text = "...";
             this.btnBrowse.UseVisualStyleBackColor = true;
@@ -79,10 +79,10 @@
             // btnFixDocument
             // 
             this.btnFixDocument.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnFixDocument.Location = new System.Drawing.Point(1288, 8);
-            this.btnFixDocument.Margin = new System.Windows.Forms.Padding(6);
+            this.btnFixDocument.Location = new System.Drawing.Point(966, 6);
+            this.btnFixDocument.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFixDocument.Name = "btnFixDocument";
-            this.btnFixDocument.Size = new System.Drawing.Size(172, 44);
+            this.btnFixDocument.Size = new System.Drawing.Size(129, 33);
             this.btnFixDocument.TabIndex = 3;
             this.btnFixDocument.Text = "Fix Document";
             this.btnFixDocument.UseVisualStyleBackColor = true;
@@ -92,20 +92,20 @@
             // 
             this.lstOutput.FormattingEnabled = true;
             this.lstOutput.HorizontalScrollbar = true;
-            this.lstOutput.ItemHeight = 25;
-            this.lstOutput.Location = new System.Drawing.Point(30, 62);
-            this.lstOutput.Margin = new System.Windows.Forms.Padding(6);
+            this.lstOutput.ItemHeight = 20;
+            this.lstOutput.Location = new System.Drawing.Point(22, 46);
+            this.lstOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstOutput.Name = "lstOutput";
-            this.lstOutput.Size = new System.Drawing.Size(1426, 704);
+            this.lstOutput.Size = new System.Drawing.Size(1070, 524);
             this.lstOutput.TabIndex = 4;
             // 
             // btnCopy
             // 
             this.btnCopy.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnCopy.Location = new System.Drawing.Point(1240, 777);
-            this.btnCopy.Margin = new System.Windows.Forms.Padding(6);
+            this.btnCopy.Location = new System.Drawing.Point(930, 583);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(220, 50);
+            this.btnCopy.Size = new System.Drawing.Size(165, 38);
             this.btnCopy.TabIndex = 7;
             this.btnCopy.Text = "Copy Error Details";
             this.btnCopy.UseVisualStyleBackColor = true;
@@ -113,9 +113,10 @@
             // 
             // BtnSettings
             // 
-            this.BtnSettings.Location = new System.Drawing.Point(1080, 775);
+            this.BtnSettings.Location = new System.Drawing.Point(810, 581);
+            this.BtnSettings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnSettings.Name = "BtnSettings";
-            this.BtnSettings.Size = new System.Drawing.Size(147, 50);
+            this.BtnSettings.Size = new System.Drawing.Size(110, 38);
             this.BtnSettings.TabIndex = 9;
             this.BtnSettings.Text = "Settings";
             this.BtnSettings.UseVisualStyleBackColor = true;
@@ -123,10 +124,10 @@
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1480, 848);
+            this.ClientSize = new System.Drawing.Size(1110, 636);
             this.Controls.Add(this.BtnSettings);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.lstOutput);
@@ -136,7 +137,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
